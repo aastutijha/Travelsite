286029a083369cb914657318f57cacf379b0313a
aastutijha <jhaastuti.ajha@gmail.com>
2024-04-21 12:07:58 +0545

    added new component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Main from './components/Main/Main.jsx'
+import TripWeatherForecast from './components/TripWeatherForecast/TripWeatherForecast.jsx'
 import SubMain from './components/Submain/Submain.jsx'
 import AboutUs from './components/AboutUs/AboutUs.jsx'
 import OurFacilities from './components/OurFacilities/OurFacilities.jsx'
 import Clientsection from './components/Clientsection/Clientsection.jsx'
 import ChoiceCard from './components/ChoiceCard/ChoiceCard.jsx'
+
 //import Team from './components/Team/Team.jsx'
 import Contact from './components/contact/contact.jsx'
 import InformationBar from './components/InformationBar/InformationBar.jsx'
@@ -19,6 +21,7 @@ function App() {
     <>
     <Navbar />
     <Main />
+    <TripWeatherForecast />
     <SubMain />
     <AboutUs />
     <OurFacilities/>
