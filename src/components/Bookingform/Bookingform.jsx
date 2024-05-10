329765f6359c19a7f245086a27c26c0a13679531
aastutijha <jhaastuti.ajha@gmail.com>
2024-05-10 17:59:32 +0545

    chore: Update booking form styling and animation

--- src/components/Bookingform/Bookingform.jsx
+++ src/components/Bookingform/Bookingform.jsx
@@ -1,25 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './bookingform.css';
 
-const BookingForm = ({ onClose }) => {
+const BookingForm = ({ onClose, onSubmit }) => {
+  const [formData, setFormData] = useState({
+    date: '04/16/2024',
+    destination: '',
+    adult: 0,
+    children: 0,
+    infant: 0,
+    name: '', // Adding name field to formData state
+  });
+  const [submitted, setSubmitted] = useState(false); // Track form submission
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isEmpty = Object.values(formData).some(value => value === '');
+    
+    if (isEmpty) {
+      alert('Please fill in all the details.');
+      return;
+    }
+    onSubmit(formData);
+    setSubmitted(true); // Set form submission status to true
+  };
+
+  const calculateTotalAmount = () => {
+    // Implement your logic to calculate the total amount based on the formData
+    return 0;
+  };
+
+  const ThankYouMessage = () => {
+    return (
+      <div className="thank-you-message">
+        <h2>Thank You!</h2>
+        <p>Dear {formData.name}, your booking has been confirmed.</p>
+      </div>
+    );
+  };
+
   return (
     <div className="booking-form-container">
-      <div className="booking-form">
-        <h2> </h2>
-        {/* JotForm iframe */}
-        <iframe
-          title="JotForm Template"
-          src="https://form.jotform.com/241301239793455"
-          frameborder="0"
-          style={{ width: '100%', height: '100%' }}
-        ></iframe>
-        {/* End of JotForm iframe */}
-        <div className="button-group">
-          <button className="close-button" onClick={onClose}>
-            Close
-          </button>
+      {submitted ? (
+        <ThankYouMessage />
+      ) : (
+        <div className="booking-form">
+          <h2> Book a Tour </h2>
+         
+          <div className="form-group">
+            <label>Tour Date</label>
+            <input
+              type="date"
+              name="date"
+              value={formData.date}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Destination</label>
+            <input
+              type="text"
+              name="destination"
+              value={formData.destination}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Adult</label>
+            <select name="adult" value={formData.adult} onChange={handleChange}>
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              {/* Add more options as needed */}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Children</label>
+            <select
+              name="children"
+              value={formData.children}
+              onChange={handleChange}
+            >
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              {/* Add more options as needed */}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Infant 0-3 yrs</label>
+            <select name="infant" value={formData.infant} onChange={handleChange}>
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              {/* Add more options as needed */}
+            </select>
+          </div>
+          <div className="total-amount">
+            <span>Total Amount:</span>
+            <span>{calculateTotalAmount()}</span>
+          </div>
+          <div className="button-group">
+            <button type="submit" onClick={handleSubmit} className="book-now">
+              Book Now
+            </button>
+            <button className="close-button" onClick={onClose}>
+              Close
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
---
+++ src/components/Submain/Tours/Inbound/Navbar.jsx
@@ -0,0 +1,227 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-scroll";
+import { FaBars } from "react-icons/fa"; // Import the hamburger icon
+import "./navbar.css";
+import company from "../../assets/logo.png";
+
+const Navbar = () => {
+  const [theme] = useState("light-mode");
+  const [showMenu, setShowMenu] = useState(false); // Set initial state to false
+  const menuRef = useRef();
+
+  useEffect(() => {
+    // Close the menu by default when the component mounts
+    setShowMenu(true);
+    document.body.classList.remove("showMenu"); // Remove 'showMenu' class
+  }, []);
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  useEffect(() => {
+    const closeMenu = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    return () => {
+      document.removeEventListener("click", closeMenu);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+    document.body.classList.toggle("showMenu", showMenu);
+  };
+  window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    var scrollTop = window.scrollY;
+
+    // Change background color based on scroll position
+    if (scrollTop === 0) {
+        navbar.classList.add('transparent-box'); // Add transparent background class
+    } else {
+        navbar.classList.remove('transparent-box'); // Remove transparent background class
+    }
+});
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="main" spy={true} smooth={true} duration={500}>
+          <img
+            src={theme === "light-mode" ? company : companyInv}
+            alt="Logo"
+            className="logoImage"
+          />
+        </Link>
+
+        <div className="name">Travel holidays</div>
+      </div>
+
+      {/* this is main desktop view navbar */}
+      <div className="middle">
+        <div className={`navItems ${showMenu ? "show" : ""}`}>
+          <Link
+            activeClass="active"
+            to="main"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={400}
+            className="navItem"
+          >
+            Home
+          </Link>
+          <Link
+            activeClass="active"
+            to="sub-main"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={400}
+            className="navItem"
+          >
+            Places
+          </Link>
+          <Link
+            activeClass="active"
+            to="aboutus"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={400}
+            className="navItem"
+          >
+            About
+          </Link>
+          <Link
+            activeClass="active"
+            to="clients-section"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={400}
+            className="navItem"
+          >
+            Clients
+          </Link>
+          <Link
+            activeClass="active"
+            to="ourfacilities"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={400}
+            className="navItem"
+          >
+            Facilities
+          </Link>
+          <Link
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={400}
+            className="navItem"
+          >
+            Contact
+          </Link>
+          <Link
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={400}
+            className="navItem"
+          >
+            Login
+          </Link>
+        </div>
+      </div>
+      <div className="hamburger" onClick={toggleMenu} ref={menuRef}>
+        <FaBars />
+        <div className={` ${showMenu ? "showMobMenu" : "myMobMenu"} `}>
+          <Link
+            onClick={toggleMenu}
+            activeClass="active"
+            to="main"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={400}
+            className="mobItem"
+          >
+            Home
+          </Link>
+          <Link
+            onClick={toggleMenu}
+            activeClass="active"
+            to="sub-main"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={400}
+            className="mobItem"
+          >
+            Places
+          </Link>
+          <Link
+            onClick={toggleMenu}
+            activeClass="active"
+            to="aboutus"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={400}
+            className="mobItem"
+          >
+            About
+          </Link>
+          <Link
+            onClick={toggleMenu}
+            activeClass="active"
+            to="clients-section"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={400}
+            className="mobItem"
+          >
+            Clients
+          </Link>
+          <Link
+            onClick={toggleMenu}
+            activeClass="active"
+            to="ourfacilities"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={400}
+            className="mobItem"
+          >
+            Facilities
+          </Link>
+          <Link
+            onClick={toggleMenu}
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={400}
+            className="mobItem"
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
